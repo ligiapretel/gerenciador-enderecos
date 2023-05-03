8be1ed8d7b821a2cc2831f366c93881e9cbd5c84
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2023-05-03 10:00:38 -0300

    Criando novo registro de usuário no backend, a partir do componente UserRegisterScreen

--- frontend/src/screens/UserRegisterScreen/index.jsx
+++ frontend/src/screens/UserRegisterScreen/index.jsx
@@ -1,6 +1,7 @@
 import './userRegisterScreen.css';
 import Header from '../../components/Header';
 import { useState } from 'react';
+import api from '../../api/api';
 
 function UserRegisterScreen(){
 
@@ -8,11 +9,18 @@ function UserRegisterScreen(){
   const [email, setEmail] = useState('');
  
 
-    function handleSubmit(event){
+    async function handleSubmit(event){
         event.preventDefault();
+
+        await api.post('/users',{
+          name,
+          email,
+        })
 
         setName('');
         setEmail('');
+
+        alert('Usuário cadastrado');
 
       }
     
